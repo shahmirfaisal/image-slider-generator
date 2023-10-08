09b8d5b2bf7e7b35887192521ff61adbe3d7bd1a
Shahmir Faisal <shahmir049@gmail.com>
2023-10-08 12:38:43 +0500

    Added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,31 @@
-import "../styles/global.css";
-import Head from "next/head";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider } from "@emotion/react";
-import theme from "../src/theme";
-import { useRouter } from "next/router";
-import createEmotionCache from "../src/createEmotionCache";
-import { useEffect } from "react";
+import "../styles/global.css"
+import Head from "next/head"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider } from "@emotion/react"
+import theme from "../src/theme"
+import { useRouter } from "next/router"
+import createEmotionCache from "../src/createEmotionCache"
+import { useEffect } from "react"
+import { Analytics } from "@vercel/analytics/react"
 
-const clientSideEmotionCache = createEmotionCache();
+const clientSideEmotionCache = createEmotionCache()
 
 const App = (props) => {
-  const router = useRouter();
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const router = useRouter()
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
   useEffect(() => {
     const handleRouteChange = (url) => {
       window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      });
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
+        page_path: url
+      })
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <CacheProvider value={emotionCache}>
@@ -35,9 +36,10 @@ const App = (props) => {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </CacheProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
